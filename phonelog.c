bee67fe429ade212a84a5e6b348d43c40999a078
Rukmani Shahi <155186677+rukmanishahi@users.noreply.github.com>
2026-01-10 07:39:33 +0000

    c code using struct and still in progress

---
+++ phonelog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <strg.h>
+struct nu(){
+    char name[50];
+    int num;
+};
+char menu(){
+
+    printf("would u like to view the phone book? (y or n)\n");
+    if (scanf(%c)=y){
+        //disp the log book
+        printf("--------------------\n",nu.name,nu.num);
+    }
+    else{ main();
+    }
+
+}
+int main(){}
+    struct nu logs;
+    printf("enter the name\n");
+    scanf("%s\n",&logs.name);
+    printf("enter the phone number\n");
+    scanf("%d\n",&logs.num);
+    menu(logs);
+
+}
